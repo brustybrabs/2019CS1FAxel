6db7f493fe3c97f60e9666db56e44080e5906830
brustybrabs <agarcia21@nscds.org>
2019-12-05 21:05:26 +0000

    I added a new java code

--- Final Project 3/level3.js
+++ Final Project 3/level3.js
@@ -1,6 +1,23 @@
-function normalize(n) {
-  for(var x = 0, i = 0; i < n; i++){
-    x += Math.random("button");
-  }
-  return (x / n);
-}
+(function makeDiv(){
+    
+    var divsize = ((Math.random()*100) + 50).toFixed();
+    var color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
+    $newdiv = $('button').css({
+        'width':divsize+'55px',
+        'height':divsize+'17px',
+        'background-color': color
+    });
+
+    var posx = (Math.random() * ($(document).width('500px') - divsize)).toFixed();
+    var posy = (Math.random() * ($(document).height('500px') - divsize)).toFixed();
+
+    $newdiv.css({
+        'position':'absolute',
+        'left':posx+'400px',
+        'top':posy+'50px',
+        'display':'none'
+    }).appendTo( 'body' ).fadeIn(100).delay(1000).fadeOut(500, function(){
+      $(this).remove();
+      makeDiv(); 
+    }); 
+})()
