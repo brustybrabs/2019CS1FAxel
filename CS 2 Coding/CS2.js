271e9ceb783751107d6509c26f69b5ea14945f59
brustybrabs <agarcia21@nscds.org>
2020-01-17 15:14:38 +0000

    challenge

---
+++ CS 2 Coding/CS2.js
@@ -0,0 +1,54 @@
+/*function add(param1, param2){
+    return param1 + param2;
+}
+
+var a = 3;
+var b = 5;
+
+console.log(a + "+" + b + " = " + add(a,b));
+
+/*
+* Write three other functions for multiplying, subtracting, and dividing and
+* test the functions as shown above.
+*/
+/*function multiply(param1,param2){
+    return param1*param2;
+}
+
+var a = 3;
+var b = 5;
+
+console.log(a + "*" + b + " = " + multiply(a,b));
+
+function multiply (param1,param2){
+    return param1/param2;
+}
+
+var a = 15;
+var b = 5;
+
+console.log(a + "/" + b + "=" + multiply(a,b));
+
+function multiply (param1,param2){
+    return param1-param2;
+}
+
+var a = 8;
+var b = 1;
+
+console.log(a+"-"+b+"="+multiply(a,b))*/
+
+function showNumbers(min,high){
+ for(var i=min; i<high; i++){
+    console.log(i);
+ }
+}
+ showNumbers(0,10);
+
+
+ /*Outputs the numbers 0,1,2,3,4,5,6,7,8,9
+ 
+ Write a function that takes two numbers, a min and a high, and outputs the numbers 
+ between the min and high. The function declaration has been done for you.*/
+
+
