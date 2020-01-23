5119851bd9d7b7c4553847fda57008af1c55f81a
brustybrabs <agarcia21@nscds.org>
2020-01-23 19:18:11 +0000

    challenge3

--- CS 2.2/challenge 2.js
+++ CS 2.2/challenge 2.js
@@ -7,12 +7,14 @@
 console.log(centuryFromYear(1700));
 console.log(centuryFromYear(1905));*/
 
-function reverse(message){
+function reverse(message) {
     var reverseString= "";
      for (var i=message.length-1; i>=0; i--){
-         reverseString += message.chartAt(i);
+         reverseString += message.charAt(i);
      }
      return reverseString;
 }
-console.log(reverse("hello"));
+console.log(reverse("sofia"));
 console.log(reverse("good"));
+
+
