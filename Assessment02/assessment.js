e34fcd1ba917c8ef1f975621ef647f7b6f3b6d7b
brustybrabs <agarcia21@nscds.org>
2020-03-09 18:17:49 +0000

    assessment

---
+++ Assessment02/assessment.js
@@ -0,0 +1,23 @@
+$('.orange').click(
+    function(){
+      var color= $('.orange').css("background-color");
+      console.log(color);
+      $('.bar').css("background-color",color);
+    }
+); 
+
+$('.purple').click(
+    function(){
+        var color= $('.purple').css("background-color");
+        console.log(color);
+        $('.bar').css("background-color",color);
+    }
+);
+
+$('.red').click(
+    function(){
+        var color= $('.red').css("background-color");
+        console.log(color);
+        $('.bar').css("background-color",color);
+    }
+);
